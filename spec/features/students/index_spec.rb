aeb72e311c9b5375f58015aa84ea90e352b63760
Paul Debevec <paulmdebevec@gmail.com>
2019-12-06 09:50:18 -0700

    Corrected test layout issues

--- spec/features/students/index_spec.rb
+++ spec/features/students/index_spec.rb
@@ -1,21 +1,15 @@
-# As a visitor,
-# When I visit '/students',
-# I see a list of students with the following information:
-# -Name
-# -Age
-# -House
-# (e.g. "Name: Casseopia Black, Age: 14, House: Slytherin")
+require 'rails_helper'
 
 RSpec.describe "When I visit /students " do
   it "I see a list with the following information
     - Name
     - Age
     - House" do
-
     student = Student.create!(name: "Casseopia Black", age: 14, house: "Slytherin")
-  end
-  visit '/students'
+
+    visit '/students'
   expect(page).to have_content(student.name)
   expect(page).to have_content(student.age)
   expect(page).to have_content(student.house)
+end
 end
